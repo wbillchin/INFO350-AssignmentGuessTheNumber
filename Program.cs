926a3d71698f0dc7ab0253c4d9a5db500df26d1f
Bill Chin <chinww@vcu.edu>
2024-09-18 18:46:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * INFO 350 - Assignment Guess the Number
+ * 
+ * <replace with your name>
+ * 
+ * Write a program in C# that asks the user to guess the number
+ * that is set as correctNumber.
+ * 
+ *  - Keep looping until the user guesses the right number
+ *  - Use all the console messages below correctly
+ *  - How to read input is given below, incorporate into your finished product
+ * 
+ * 
+ * Modify this code to complete this assignment
+ */
+
+using System;
+class GuessTheNumberGame
+{
+    static void Main(string[] args)
+    {
+        int correctNumber = 7;
+        int guess = 0;
+        int attempts = 0;
+
+        Console.WriteLine("You are playing Guess the Number");
+        Console.WriteLine("I'm thinking of a number between 1 and 10.");
+
+
+
+        //Console.Write("Enter your guess: ");
+
+        /*
+         *  This is how to read in from the console
+         *  string input = Console.ReadLine();
+         */
+
+        /*
+         *  Convert from that input string to an int
+         *   int.TryParse(input, out guess)
+         *   
+         */
+
+
+        //Console.WriteLine("Too high! Try again.");
+        //Console.WriteLine("Too low! Try again.");
+        //Console.WriteLine("Too low! Try again.");
+        //Console.WriteLine($"Congratulations! You guessed the correct number in {attempts} attempts!");
+        //Console.WriteLine("Please enter a valid number.");
+
+
+    }
+}
